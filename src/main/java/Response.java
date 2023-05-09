834b07769d77227bad5cebcce145f4106139f942
cooparo <pparolini1@gmail.com>
2023-05-09 22:09:09 +0200

    Some casual test

---
+++ src/main/java/Response.java
@@ -0,0 +1,85 @@
+import java.util.ArrayList;
+
+public class Response {
+    private String status;
+    private String userTier;
+    private int total;
+    private int startIndex;
+    private int pageSize;
+    private int currentPage;
+    private int pages;
+    private String orderBy;
+    private ArrayList<Article> results;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getUserTier() {
+        return userTier;
+    }
+
+    public void setUserTier(String userTier) {
+        this.userTier = userTier;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public int getStartIndex() {
+        return startIndex;
+    }
+
+    public void setStartIndex(int startIndex) {
+        this.startIndex = startIndex;
+    }
+
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public int getCurrentPage() {
+        return currentPage;
+    }
+
+    public void setCurrentPage(int currentPage) {
+        this.currentPage = currentPage;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public String getOrderBy() {
+        return orderBy;
+    }
+
+    public void setOrderBy(String orderBy) {
+        this.orderBy = orderBy;
+    }
+
+    public ArrayList<Article> getResults() {
+        return results;
+    }
+
+    public void setResults(ArrayList<Article> results) {
+        this.results = results;
+    }
+}
---
+++ src/main/java/Root.java
@@ -0,0 +1,11 @@
+public class Root {
+    private Response response;
+
+    public Response getResponse() {
+        return response;
+    }
+
+    public void setResponse(Response response) {
+        this.response = response;
+    }
+}
