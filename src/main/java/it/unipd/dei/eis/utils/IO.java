8600bac12453859fad5cca569a577b87c0286002
andreapdn <andrea.padovan25@gmail.com>
2023-05-26 17:28:48 +0200

    Implemented IO to read/write files faster

---
+++ src/main/java/it/unipd/dei/eis/utils/IO.java
@@ -0,0 +1,19 @@
+package it.unipd.dei.eis.utils;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+public class IO {
+    public static String readFile(String path) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(path)), StandardCharsets.UTF_8);
+    }
+    public static void writeFile(String path, String data) throws IOException {
+        Files.write(Paths.get(path),
+                data.getBytes(StandardCharsets.UTF_8),
+                StandardOpenOption.CREATE,
+                StandardOpenOption.TRUNCATE_EXISTING);
+    }
+}
