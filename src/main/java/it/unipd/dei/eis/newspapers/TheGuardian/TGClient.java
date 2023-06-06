8af73d7ace495f4d17db2e21b92421e490adedff
andreapdn <andrea.padovan25@gmail.com>
2023-06-06 17:29:01 +0200

    Removed getApiKey() in TGClient

--- src/main/java/it/unipd/dei/eis/newspapers/TheGuardian/TGClient.java
+++ src/main/java/it/unipd/dei/eis/newspapers/TheGuardian/TGClient.java
@@ -61,10 +61,6 @@ public class TGClient {
         return results;
     }
 
-    public String getApiKey() {
-        return apiKey;
-    }
-
     public void setApiKey(String apiKey) {
         this.apiKey = apiKey;
     }
